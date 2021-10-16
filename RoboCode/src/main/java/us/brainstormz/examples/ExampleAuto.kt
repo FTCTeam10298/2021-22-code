7b992c34dd71d05d6d99c027712103b5250bc3a9
james <sj2@penrose.us>
2021-10-16 11:34:39 -0700

    example auto and hardware

--- RoboCode/src/main/java/us/brainstormz/examples/ExampleAuto.kt
+++ RoboCode/src/main/java/us/brainstormz/examples/ExampleAuto.kt
@@ -1,5 +1,23 @@
 package us.brainstormz.examples
 
-class ExampleAuto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import us.brainstormz.choivico.robotCode.hardwareClasses.EncoderDriveMovement
+import us.brainstormz.choivico.telemetryWizard.TelemetryConsole
+
+@Autonomous
+class ExampleAuto: LinearOpMode() {
+    val hardware = ExampleHardware()
+    val movement = EncoderDriveMovement(hardware, TelemetryConsole(telemetry))
+    override fun runOpMode() {
+        /** INIT PHASE */
+        hardware.init(hardwareMap)
+
+        waitForStart()
+        /** AUTONOMOUS  PHASE */
+        movement.driveRobotPosition(power = 1.0, inches = 20.0, smartAccel = true)
+        movement.driveRobotStrafe(power = 1.0, inches = 20.0, smartAccel = true)
+        movement.driveRobotTurn(power = 1.0, degree = 20.0, smartAccel = true)
+    }
 
 }
