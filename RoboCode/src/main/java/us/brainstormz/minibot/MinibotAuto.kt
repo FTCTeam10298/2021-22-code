3de1adabda24d0d6de952cfcc26d3221fa899215
james <sj2@penrose.us>
2021-11-11 14:35:23 -0800

    created auto and teamMarkerDetector

---
+++ RoboCode/src/main/java/us/brainstormz/minibot/MinibotAuto.kt
@@ -0,0 +1,33 @@
+package us.brainstormz.minibot
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import us.brainstormz.openCvAbstraction.OpenCvAbstraction
+import us.brainstormz.choivico.ringDetector.RingDetector
+import us.brainstormz.hardwareClasses.EncoderDriveMovement
+import us.brainstormz.telemetryWizard.TelemetryConsole
+import us.brainstormz.telemetryWizard.TelemetryWizard
+
+@TeleOp(name="Minibot Auto", group="Minibot")
+class MinibotAuto: LinearOpMode() {
+
+    val hardware = MinibotHardware()
+    val movement = EncoderDriveMovement(hardware, TelemetryConsole(telemetry))
+    val depositor = Depositor(hardware, telemetry)
+
+    val wizard = TelemetryWizard(TelemetryConsole(telemetry), this)
+
+    val opencv = OpenCvAbstraction(this)
+    val ringDetector = RingDetector(150, 135, TelemetryConsole(telemetry))
+
+    override fun runOpMode() {
+        /** INIT PHASE */
+        hardware.init(hardwareMap)
+
+        waitForStart()
+        /** AUTONOMOUS  PHASE */
+//        movement.driveRobotPosition(power = 1.0, inches = 20.0, smartAccel = true)
+//        movement.driveRobotStrafe(power = 1.0, inches = 20.0, smartAccel = true)
+//        movement.driveRobotTurn(power = 1.0, degree = 20.0, smartAccel = true)
+    }
+}
