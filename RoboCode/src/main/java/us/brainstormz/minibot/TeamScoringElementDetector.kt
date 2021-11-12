9c2b8fe12874737130947117ac679f820ca57248
james <sj2@penrose.us>
2021-11-11 22:07:45 -0800

    rewrote ring detector for tse detection

--- RoboCode/src/main/java/us/brainstormz/minibot/TeamScoringElementDetector.kt
+++ RoboCode/src/main/java/us/brainstormz/minibot/TeamScoringElementDetector.kt
@@ -5,98 +5,92 @@ import org.opencv.imgproc.Imgproc
 import us.brainstormz.telemetryWizard.TelemetryConsole
 
 class TeamScoringElementDetector(val tseThreshold : Int, private val console: TelemetryConsole) {
-    private val fourRingThreshold : Int = 0
+    companion object {
+        val blue = Scalar(0.0, 0.0, 255.0)
+        val red = Scalar(225.0, 0.0, 0.0)
+        val black = Scalar(255.0, 255.0, 255.0)
+        val TRANSPARENT = Scalar(0.0, 255.0, 0.0)
+    }
 
-    /*
-     * An enum to define the position
-     */
+    data class Region(val rect: Rect, var cb: Mat?, var analysis: Int)
     enum class TSEPosition {
         One, Two, Three
     }
 
-    lateinit var REGION1_TOPLEFT_ANCHOR_POINT: Point
+    val region1PointA = Point(290.0, 197.0)
+    val region1PointB = Point(325.0, 222.0)
+    private val region1 = Region(Rect(region1PointA, region1PointB), null, 0)
 
-    lateinit var region1_pointA: Point
-    lateinit var region1_pointB: Point
+    val region2PointA = Point(290.0, 197.0)
+    val region2PointB = Point(325.0, 222.0)
+    private val region2 = Region(Rect(region2PointA, region2PointB), null, 0)
 
-    /*
-     * Working variables
-     */
-    var region1_Cb: Mat? = null
-    var YCrCb = Mat()
-    var Cb = Mat()
-    var analysis = 0
+    val region3PointA = Point(290.0, 197.0)
+    val region3PointB = Point(325.0, 222.0)
+    private val region3 = Region(Rect(region3PointA, region3PointB), null, 0)
 
+    var yCrCb = Mat()
+    var cb = Mat()
 
-    // Volatile since accessed by OpMode thread w/o synchronization
-    @Volatile
+    @Volatile // Volatile since accessed by OpMode thread w/o synchronization
     var position = TSEPosition.One
 
-    /*
-     * This function takes the RGB frame, converts to YCrCb,
-     * and extracts the Cb channel to the 'Cb' variable
-     */
-    fun inputToCb(input: Mat?) {
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb)
-        Core.extractChannel(YCrCb, Cb, 1)
-    }
-
     fun init(firstFrame: Mat) {
-        region1_pointA = Point(
-            REGION1_TOPLEFT_ANCHOR_POINT.x,
-            REGION1_TOPLEFT_ANCHOR_POINT.y)
-        region1_pointB = Point(
-            REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT)
-
+//        initializes cb and yCrCb
         inputToCb(firstFrame)
-        region1_Cb = Cb.submat(Rect(region1_pointA, region1_pointB))
-    }
 
+//        creates the rect
+        region1.cb = cb.submat(region1.rect)
+        region2.cb = cb.submat(region2.rect)
+        region3.cb = cb.submat(region3.rect)
+    }
 
     fun processFrame(input: Mat): Mat {
-        console.display(10, position.toString())
+//        convert input to cb color format
         inputToCb(input)
-        analysis = Core.mean(region1_Cb).`val`[0].toInt()
-        Imgproc.rectangle(
-            input,  // Buffer to draw on
-            region1_pointA,  // First point which defines the rectangle
-            region1_pointB,  // Second point which defines the rectangle
-            BLUE,  // The color the rectangle is drawn in
-            2) // Thickness of the rectangle lines
-        position = TSEPosition.One // Record our analysis
+
+//        makes a rect on the screen
+        Imgproc.rectangle(input, region1.rect, blue, 2)
+        Imgproc.rectangle(input, region2.rect, black, 2)
+        Imgproc.rectangle(input, region2.rect, red, 2)
+
+//        finds the color in the rect
+        region1.analysis = colorInRect(region1.cb!!)
+        region2.analysis = colorInRect(region2.cb!!)
+        region3.analysis = colorInRect(region3.cb!!)
+
+//        interpreting analysis
         position = when {
-//            analysis > fourRingThreshold -> {
-//                TSEPosition.One
-//            }
-            analysis > tseThreshold -> {
+            region1.analysis > tseThreshold -> {
+                TSEPosition.One
+            }
+            region2.analysis > tseThreshold -> {
                 TSEPosition.Two
+            }
+            region3.analysis > tseThreshold -> {
+                TSEPosition.Three
             }
             else -> {
                 TSEPosition.Three
             }
         }
-        Imgproc.rectangle(
-            input,  // Buffer to draw on
-            region1_pointA,  // First point which defines the rectangle
-            region1_pointB,  // Second point which defines the rectangle
-            TRANSPARENT,  // The color the rectangle is drawn in
-            1) // Negative thickness means solid fill
+        console.display(8, "Position: $position")
+        console.display(9, "Threshold: $tseThreshold")
+
+//        gives image to the phone
         return input
     }
 
-    companion object {
-        /*
-     * Some color constants
-     */
-        val BLUE = Scalar(0.0, 0.0, 255.0)
-        val TRANSPARENT = Scalar(0.0, 255.0, 0.0)
+    private fun colorInRect(rect: Mat): Int {
+        return Core.mean(rect).`val`[0].toInt()
+    }
 
-        /*
-     * The core values which define the location and size of the sample regions
+    /**
+     * This function takes the RGB frame, converts to YCrCb,
+     * and extracts the Cb channel to the cb variable
      */
-//        Camera is landscape top left is 0,0
-        const val REGION_WIDTH = 35
-        const val REGION_HEIGHT = 25
+    private fun inputToCb(input: Mat?) {
+        Imgproc.cvtColor(input, yCrCb, Imgproc.COLOR_RGB2YCrCb)
+        Core.extractChannel(yCrCb, cb, 1)
     }
 }
