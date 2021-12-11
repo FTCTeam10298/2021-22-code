4c8dffc68e2661ec1a5d26ee82d734ae3a78b029
james <sj2@penrose.us>
2021-12-11 04:33:45 -0800

    ducc auto working on both sides (42p max)

--- RoboCode/src/main/java/us/brainstormz/rataTony/RataTonyAuto.kt
+++ RoboCode/src/main/java/us/brainstormz/rataTony/RataTonyAuto.kt
@@ -35,8 +35,8 @@ class RataTonyAuto: LinearOpMode() {
         opencv.start()
         opencv.onNewFrame(tseDetector::processFrame)
 
-        wizard.newMenu("Alliance", "Which alliance are we on?", listOf("Blue", "Red"), "Starting Position", firstMenu = true)
-        wizard.newMenu("Starting Position", "Which side are we starting on?", listOf("Warehouse", "Ducc"))
+        wizard.newMenu("Alliance", "Which alliance are we on?", listOf("Blue", "Red"), "StartPos", firstMenu = true)
+        wizard.newMenu("StartPos", "Which are we closer to?", listOf("Warehouse", "Ducc"))
         wizard.summonWizard(gamepad1)
 
         console.display(1, "Initialization Complete")
@@ -51,65 +51,169 @@ class RataTonyAuto: LinearOpMode() {
             else -> depositor.midGoalHeight
         }
 
-        if (wizard.wasItemChosen("Alliance", "Blue")) {
+        when {
+            wizard.wasItemChosen("Alliance", "Blue") -> {
+                when {
+                    wizard.wasItemChosen("StartPos", "Ducc") -> {
+                        movement.driveRobotPosition(1.0, -3.0, true)
+                        movement.driveRobotStrafe(0.8, 50.0, true)
+                        movement.driveRobotStrafe(1.0, -2.0, true)
+                        movement.driveRobotTurn(1.0, -90.0, true)
+                        movement.driveRobotStrafe(1.0, 3.5, true)
+                        if (tsePosition != TSEPosition.One)
+                            movement.driveRobotStrafe(1.0, 0.7, true)
+                        depositor.yToPosition(level)
+                        deposit(level)
+
+                        if (tsePosition != TSEPosition.Three)
+                            movement.driveRobotPosition(1.0, -7.0, true)
+
+                        movement.driveRobotStrafe(1.0, -2.0, true)
+                        movement.driveRobotTurn(1.0, 90.0, true)
+                        movement.driveRobotPosition(0.5, 35.0, true)
+
+                        if (tsePosition == TSEPosition.Three)
+                            movement.driveRobotStrafe(0.3, -39.0, true)
+                        else
+                            movement.driveRobotStrafe(0.3, -32.0, true)
+
+                        hardware.carouselSpinner.power = 1.0
+                        sleep(4000)
+                        hardware.carouselSpinner.power = 0.0
+
+                        movement.driveRobotStrafe(1.0, 25.0, true)
+                        movement.driveRobotPosition(1.0, 10.0, true)
+                    }
+                    wizard.wasItemChosen("StartPos", "Warehouse") -> {
 //        deliver
-            movement.driveRobotStrafe(1.0, 5.0, true)
-            movement.driveRobotTurn(1.0,40.0,true)
-            depositor.yToPosition(level)
-            movement.driveRobotStrafe(1.0, 11.0, true)
-            movement.driveRobotPosition(1.0, 5.0, true)
-            movement.driveRobotStrafe(1.0, 15.0, true)
-            movement.driveRobotPosition(1.0, -9.0, true)
-            movement.driveRobotStrafe(1.0, 9.0, true)
-            hardware.horiServo.power = 1.0
-            sleep(2000)
-            hardware.horiServo.power = 0.0
-            sleep(1000)
-            depositor.drop()
-            sleep(1000)
-            depositor.yToPositionBlocking(level+60)
-            hardware.horiServo.power = -1.0
-            sleep(1000)
-            depositor.close()
-            hardware.horiServo.power = -1.0
-            sleep(1000)
-            hardware.horiServo.power = 0.0
-            depositor.yToPosition(depositor.state)
+                        movement.driveRobotStrafe(1.0, 5.0, true)
+                        movement.driveRobotTurn(1.0,40.0,true)
+                        depositor.yToPosition(level)
+                        movement.driveRobotStrafe(1.0, 11.0, true)
+                        movement.driveRobotPosition(1.0, 5.0, true)
+                        movement.driveRobotStrafe(1.0, 15.0, true)
+                        movement.driveRobotPosition(1.0, -9.0, true)
+                        movement.driveRobotStrafe(1.0, 9.0, true)
+                        hardware.horiServo.power = 1.0
+                        sleep(2000)
+                        hardware.horiServo.power = 0.0
+                        sleep(1000)
+                        depositor.drop()
+                        sleep(1000)
+                        depositor.yToPositionBlocking(level+60)
+                        hardware.horiServo.power = -1.0
+                        sleep(1000)
+                        depositor.close()
+                        hardware.horiServo.power = -1.0
+                        sleep(1000)
+                        hardware.horiServo.power = 0.0
+                        depositor.yToPosition(0)
 //        go to warehouse
-            movement.driveRobotStrafe(1.0, -10.0, true)
-            movement.driveRobotTurn(1.0,-40.0,true)
-            movement.driveRobotStrafe(1.0, -37.0, true)
-            movement.driveRobotPosition(1.0,-49.0,true)
+                        movement.driveRobotStrafe(1.0, -10.0, true)
+                        movement.driveRobotTurn(1.0,-40.0,true)
+                        movement.driveRobotStrafe(1.0, -37.0, true)
+                        movement.driveRobotPosition(1.0,-49.0,true)
+                    }
+                }
+            }
+            wizard.wasItemChosen("Alliance", "Red") -> {
+                when {
+                    wizard.wasItemChosen("StartPos", "Ducc") -> {
+                        movement.driveRobotPosition(1.0, 3.0, true)
+                        if (tsePosition == TSEPosition.One) {
+                            movement.driveRobotStrafe(0.8, 50.0, true)
+                            movement.driveRobotStrafe(1.0, -2.0, true)
+                        } else {
+                            movement.driveRobotStrafe(0.8, 55.0, true)
+                            movement.driveRobotStrafe(1.0, -7.0, true)
+                        }
+
+                        movement.driveRobotTurn(1.0, 90.0, true)
+                        movement.driveRobotStrafe(1.0, 5.5, true)
+                        if (tsePosition != TSEPosition.One)
+                            movement.driveRobotStrafe(1.0, 1.0, true)
+                        depositor.yToPosition(level)
+                        deposit(level)
+
+                        if (tsePosition != TSEPosition.One)
+                            movement.driveRobotPosition(1.0, 7.0, true)
+
+                        movement.driveRobotStrafe(0.8, -39.0, true)
+
+                        if (tsePosition == TSEPosition.One)
+                            movement.driveRobotPosition(0.4, 36.0, true)
+                        else
+                            movement.driveRobotPosition(0.4, 29.0, true)
+                        movement.driveRobotPosition(0.2, 3.0, true)
+
+                        hardware.carouselSpinner.power = -1.0
+                        sleep(4000)
+                        hardware.carouselSpinner.power = 0.0
+
+                        movement.driveRobotPosition(1.0, -20.0, true)
+                        movement.driveRobotStrafe(1.0, -5.0, true)
+                    }
+                    wizard.wasItemChosen("StartPos", "Warehouse") -> {
+        //          deliver
+                        movement.driveRobotStrafe(1.0, 5.0, true)
+                        movement.driveRobotTurn(1.0, -33.0, true)
+                        depositor.yToPosition(level)
+                        movement.driveRobotStrafe(1.0, 11.0, true)
+                        movement.driveRobotPosition(1.0, -5.0, true)
+                        movement.driveRobotStrafe(1.0, 15.0, true)
+                        movement.driveRobotPosition(1.0, 6.5, true)
+                        movement.driveRobotStrafe(1.0, 8.0, true)
+                        hardware.horiServo.power = 1.0
+                        sleep(2000)
+                        hardware.horiServo.power = 0.0
+                        sleep(1000)
+                        depositor.drop()
+                        sleep(1000)
+                        depositor.yToPositionBlocking(level + 100)
+                        hardware.horiServo.power = -1.0
+                        sleep(1000)
+                        depositor.close()
+                        hardware.horiServo.power = -1.0
+                        sleep(1000)
+                        hardware.horiServo.power = 0.0
+                        depositor.yToPosition(0)
+        //        go to warehouse
+                        movement.driveRobotStrafe(1.0, -10.0, true)
+                        movement.driveRobotTurn(1.0, 40.0, true)
+                        movement.driveRobotStrafe(1.0, -37.0, true)
+                        movement.driveRobotPosition(1.0, 47.0, true)
+                    }
+                }
+            }
+
         }
-        if (wizard.wasItemChosen("Alliance", "Red")) {
-//          deliver
-            movement.driveRobotStrafe(1.0, 5.0, true)
-            movement.driveRobotTurn(1.0,-33.0,true)
-            depositor.yToPosition(level)
-            movement.driveRobotStrafe(1.0, 11.0, true)
-            movement.driveRobotPosition(1.0, -5.0, true)
-            movement.driveRobotStrafe(1.0, 15.0, true)
-            movement.driveRobotPosition(1.0, 6.5, true)
-            movement.driveRobotStrafe(1.0, 8.0, true)
-            hardware.horiServo.power = 1.0
-            sleep(2000)
-            hardware.horiServo.power = 0.0
-            sleep(1000)
-            depositor.drop()
-            sleep(1000)
-            depositor.yToPositionBlocking(level + 100)
-            hardware.horiServo.power = -1.0
-            sleep(1000)
-            depositor.close()
-            hardware.horiServo.power = -1.0
-            sleep(1000)
-            hardware.horiServo.power = 0.0
-            depositor.yToPosition(depositor.state)
-//        go to warehouse
-            movement.driveRobotStrafe(1.0, -10.0, true)
-            movement.driveRobotTurn(1.0,40.0,true)
-            movement.driveRobotStrafe(1.0, -37.0, true)
-            movement.driveRobotPosition(1.0,47.0,true)
-        }
+
+//        make sure lift is down
+        depositor.yToPositionBlocking(300)
+        hardware.horiServo.power = 1.0
+        sleep(500)
+        hardware.horiServo.power = -1.0
+        sleep(1000)
+        hardware.horiServo.power = 0.0
+        depositor.yToPositionBlocking(0)
+    }
+
+//    out, drop, home
+    fun deposit(yLevel: Int) {
+        hardware.horiServo.power = 1.0
+        sleep(2000)
+        hardware.horiServo.power = 0.0
+        sleep(100)
+
+        depositor.drop()
+        sleep(1000)
+
+        depositor.yToPositionBlocking(yLevel+100)
+        hardware.horiServo.power = -1.0
+        sleep(800)
+        depositor.close()
+        sleep(1700)
+        hardware.horiServo.power = 0.0
+        depositor.yToPosition(0)
     }
 }
