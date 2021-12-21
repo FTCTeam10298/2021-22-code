f08dc760b01b14c1c17dd02e9b5896d5f03621aa
james <sj2@penrose.us>
2021-12-20 17:20:39 -0800

    x and y motion, r only works independantly

--- RoboCode/src/main/java/us/brainstormz/rataTony/NewEncoderMovementTest.kt
+++ RoboCode/src/main/java/us/brainstormz/rataTony/NewEncoderMovementTest.kt
@@ -5,11 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import us.brainstormz.localizer.EncoderLocalizer
 import us.brainstormz.localizer.PositionAndRotation
 import us.brainstormz.motion.MecanumMovement
+import us.brainstormz.telemetryWizard.TelemetryConsole
 
 @Autonomous(name= "NewEncoderMovementTest", group= "Tests")
 class NewEncoderMovementTest: LinearOpMode() {
     val hardware = RataTonyHardware()
-    val movement = MecanumMovement(EncoderLocalizer(hardware), hardware)
+    val console = TelemetryConsole(telemetry)
+    val movement = MecanumMovement(EncoderLocalizer(hardware, console), hardware, console)
 
     override fun runOpMode() {
         hardware.init(hardwareMap)
@@ -17,7 +19,19 @@ class NewEncoderMovementTest: LinearOpMode() {
 
         waitForStart()
 
-        movement.goToPosition(PositionAndRotation(0.0, 10.0, 0.0))
+        movement.goToPosition(PositionAndRotation(10.0, 10.0, 0.0))
+        movement.goToPosition(PositionAndRotation(-10.0, 0.0, 0.0))
+        movement.goToPosition(PositionAndRotation(0.0, -10.0, 0.0))
+        movement.goToPosition(PositionAndRotation(r= 90.0))
+
+
+//        movement.setSpeedAll(0.0, 1.0, 1.0, 0.0, 0.3)
+//        val startTime = time
+//        while (time - startTime < 20.0) {
+//            movement.localizer.recalculatePositionAndRotation()
+//            val currentPos = movement.localizer.currentPositionAndRotation()
+//            console.display(1, "Current pos: $currentPos")
+//        }
     }
 
 }
