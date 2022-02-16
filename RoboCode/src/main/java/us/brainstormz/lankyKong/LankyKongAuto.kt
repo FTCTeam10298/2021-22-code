1fc665c0ab099fee75f304c601861573c4e7b80e
james <sj2@penrose.us>
2022-02-15 22:02:00 -0800

    trying to do drive tuning

--- RoboCode/src/main/java/us/brainstormz/lankyKong/LankyKongAuto.kt
+++ RoboCode/src/main/java/us/brainstormz/lankyKong/LankyKongAuto.kt
@@ -1,6 +1,25 @@
 package us.brainstormz.lankyKong
 
-//@Autonomous(name= "Lanky Kong Auto", group= "B")
-class LankyKongAuto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.openftc.easyopencv.OpenCvCameraRotation
+import us.brainstormz.openCvAbstraction.OpenCvAbstraction
+import us.brainstormz.rataTony.RataTonyHardware
+
+@Autonomous(name= "Lanky Kong Auto", group= "B")
+class LankyKongAuto: LinearOpMode() {
+
+    val hardware = LankyKongHardware()
+    val opencv = OpenCvAbstraction(this)
+    override fun runOpMode() {
+
+        opencv.init(hardwareMap)
+        opencv.cameraName = hardware.cameraName
+        opencv.cameraOrientation = OpenCvCameraRotation.SIDEWAYS_RIGHT
+        opencv.start()
+//        opencv.onNewFrame(tseDetector::processFrame)
+
+        waitForStart()
+    }
 
 }
