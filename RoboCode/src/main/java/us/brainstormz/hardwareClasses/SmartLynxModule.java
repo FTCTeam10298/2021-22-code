530830e794e5eb59c6c8381a7d0d5534ab997938
james <sj2@penrose.us>
2022-02-26 00:44:31 -0800

    tried adding i2c on to port hardware

--- RoboCode/src/main/java/us/brainstormz/hardwareClasses/SmartLynxModule.java
+++ RoboCode/src/main/java/us/brainstormz/hardwareClasses/SmartLynxModule.java
@@ -5,6 +5,7 @@ import com.qualcomm.hardware.lynx.LynxDcMotorController;
 import com.qualcomm.hardware.lynx.LynxDigitalChannelController;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.lynx.LynxServoController;
+import com.qualcomm.hardware.lynx.commands.core.LynxI2cConfigureChannelCommand;
 import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServoImplEx;
@@ -12,7 +13,10 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DigitalChannelImpl;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.hardware.configuration.DeviceConfiguration.I2cChannel;
+import com.qualcomm.robotcore.hardware.configuration.LynxI2cDeviceConfiguration;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.ServoConfigurationType;
 
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
@@ -26,12 +30,14 @@ public class SmartLynxModule {
     private LynxServoController servoController;
     private LynxAnalogInputController lynxAnalogInputController;
     private LynxDigitalChannelController lynxDigitalChannelController;
+//    private I2cDeviceSynchDevice lynxI2cSynchDevice;
 
     private HashMap<Integer, DcMotorEx> cachedMotors;
     private HashMap<Integer, ServoImplEx> cachedServos;
     private HashMap<Integer, CRServoImplEx> cachedCRServos;
     private HashMap<Integer, AnalogInput> cachedAI;
     private HashMap<Integer, DigitalChannel> cachedDC;
+//    private HashMap<Integer, I2cChannel> cachedI2c;
 
     public SmartLynxModule(LynxModule module) {
         try {
@@ -48,6 +54,7 @@ public class SmartLynxModule {
         cachedCRServos = new HashMap<>();
         cachedAI = new HashMap<>();
         cachedDC = new HashMap<>();
+//        cachedI2c = new HashMap<>();
     }
 
     public DcMotorEx getMotor(int port){
@@ -84,4 +91,13 @@ public class SmartLynxModule {
         }
         return cachedDC.get(port);
     }
+
+
+//    public I2cChannel getI2cController(int port){
+//        int channel = 0;
+//        if(!cachedI2c.containsKey(port)){
+//            cachedI2c.put(port, new I2cChannel(channel));
+//        }
+//        return cachedI2c.get(port);
+//    }
 }
