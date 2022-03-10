096069e82f0562938d363f4c6b18a1c755676a0c
james <sj2@penrose.us>
2022-03-09 22:21:16 -0800

    ducc side auto pathing works -park

--- RoboCode/src/main/java/us/brainstormz/lankyKong/LankyKongAuto.kt
+++ RoboCode/src/main/java/us/brainstormz/lankyKong/LankyKongAuto.kt
@@ -3,6 +3,7 @@ package us.brainstormz.lankyKong
 import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.util.Range
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import us.brainstormz.hardwareClasses.EncoderDriveMovement
 import us.brainstormz.localizer.EncoderLocalizer
@@ -43,10 +44,23 @@ class LankyKongAuto: LinearOpMode() {
         wizard.newMenu("Alliance", "Which alliance are we on?", listOf("Blue", "Red"), "StartPos", firstMenu = true)
         wizard.newMenu("StartPos", "Which are we closer to?", listOf("Warehouse" to null, "Ducc" to "ParkLocation"))
         wizard.newMenu("ParkLocation", "Where to park?", listOf("Warehouse", "Storage Unit"))
-        wizard.summonWizard(gamepad1)
+//        wizard.summonWizard(gamepad1)
+
+        var initBackDistance = 0.0
+        var initFrontDistance = 0.0
+        while(!opModeIsActive()) {
+            initBackDistance = hardware.backDistance.getDistance(DistanceUnit.INCH)
+            initFrontDistance = hardware.frontDistance.getDistance(DistanceUnit.INCH)
+            console.display(2,
+                            "Front Range: $initFrontDistance \nBack Range: $initBackDistance")
+        }
 
         console.display(1, "Initialization Complete")
         waitForStart()
+
+        val initDistance = 26.8
+        oldMovement.driveRobotPosition(1.0, -(initBackDistance - initDistance), false)
+
 //        opencv.stop()
 //        val level: DepositorLK.LiftPos = when (tsePosition) {
 //            TeamScoringElementDetector.TSEPosition.One -> Depositor.LiftPos.LowGoal
@@ -54,12 +68,12 @@ class LankyKongAuto: LinearOpMode() {
 //            TeamScoringElementDetector.TSEPosition.Three -> Depositor.LiftPos.HighGoal
 //        }
 
-        oldMovement.driveRobotStrafe(1.0, 25.0, true)
+        oldMovement.driveRobotStrafe(0.8, 25.0, true)
         val preloadTurn = 30.0
         oldMovement.driveRobotTurn(1.0, preloadTurn, true)
         depo.moveToPosition(LiftPos.HighGoal.counts, 4500)
         hardware.dropperServo.position = DropperPos.Open.posValue
-        sleep(1000)
+        sleep(100)
         hardware.dropperServo.position = DropperPos.Closed.posValue
         depo.moveToPosition(
             yPosition = LiftPos.HighGoal.counts,
@@ -67,54 +81,61 @@ class LankyKongAuto: LinearOpMode() {
         depo.moveToPosition(
             yPosition = depo.fullyDown,
             xPosition = depo.xFullyRetracted)
+
+//        Spin ducc
         oldMovement.driveRobotTurn(1.0, -preloadTurn, true)
         oldMovement.driveRobotPosition(1.0, -25.0, true)
         oldMovement.driveRobotTurn(1.0, 90.0, true)
-        oldMovement.driveRobotStrafe(1.0, -13.0, true)
+        oldMovement.driveRobotStrafe(1.0, -13.0, false)
+        sleep(700)
         val frontDistance = hardware.frontDistance.getDistance(DistanceUnit.INCH)
-        val targetDistance = 9.4
+        val targetDistance = 9
+        oldMovement.driveRobotPosition(1.0, frontDistance-targetDistance, true)
         hardware.duccSpinner1.power = 1.0
-        movement.goToPosition(PositionAndRotation(x= 1.0, y= frontDistance - targetDistance), 0.0..1.0)
-        hardware.duccSpinner1.power = 0.0
-    }
-
-    //    out, drop, in
-    fun deposit(xCounts: Int, yLevel: DepositorLK.LiftPos) {
-        depo.moveTowardPosition(yLevel.counts.toDouble(), 0.0)
-        sleep(100)
-        depo.moveToPosition(yLevel.counts, xCounts)
-        depo.dropper(DepositorLK.DropperPos.Open, false)
-        sleep(500)
-        depo.dropper(DepositorLK.DropperPos.Closed, false)
-        depo.moveToPosition(yLevel.counts, 0)
-        depo.moveTowardPosition(0.0, 0.0)
-    }
-
-    fun cycle(side: Alliance, initalYIn: Double = 0.0) {
-        val drivePolarity = when (side) { Alliance.Red -> -1
-                                          Alliance.Blue -> 1 }
-        val collectorMotor = when (side) { Alliance.Red -> hardware.collector
-                                           Alliance.Blue -> hardware.collector2 }
-        val inPower = 1.0
-
-        var yAdjuster = -initalYIn
-
-/**        Stage 1: Collect */
-        val inchesToCollect = 40.0 *2
-//        movement.driveRobotHug(1.0, (-inchesToCollect + yAdjuster) * drivePolarity, true)
-
-//        collectorMotor.power = inPower
-//        movement.driveRobotPosition(0.3, 5.0, true)
-//        collectorMotor.power = 0.0
-
-/**        Stage 2: Deposit */
-        yAdjuster = inchesToCollect - hardware.frontDistance.getDistance(DistanceUnit.INCH)
-
-        val inchesToDeposit = 40.0 *2
-//        movement.driveRobotHug(1.0, (inchesToDeposit + yAdjuster) * drivePolarity, true)
-//        depo.deposit()
         sleep(1000)
+//        hardware.duccSpinner1.power = 0.5
+//        sleep(500)
+        hardware.duccSpinner1.power = 0.1
+        sleep(2000)
+        /*
+        val startTime = System.currentTimeMillis()
+        while(System.currentTimeMillis() - startTime > 4000) {
+            val dTime = System.currentTimeMillis() - startTime
+            val timeToPower = Range.scale(dTime.toDouble(), 0.0, 4.0, 1.0, 0.0)
+            hardware.duccSpinner1.power = timeToPower
+        }*/
+        hardware.duccSpinner1.power = 0.0
 
+//        collect ducc
+        hardware.collector.power = 1.0
+        oldMovement.driveRobotStrafe(1.0, 8.0, true)
+        oldMovement.driveRobotTurn(1.0,-50.0, true)
+        oldMovement.driveRobotPosition(1.0, 20.0, true)
+        oldMovement.driveRobotStrafe(1.0, -5.0, true)
+        hardware.collector2.power = 1.0
+
+//        Deliver Ducc
+        oldMovement.driveRobotTurn(1.0,10.0, true)
+        oldMovement.driveRobotStrafe(1.0, -5.0, false)
+        oldMovement.driveRobotPosition(1.0, -10.0, true)
+        hardware.collector.power = 0.0
+        hardware.collector2.power = 0.0
+        val backDistance = hardware.backDistance.getDistance(DistanceUnit.INCH)
+        val hubLineup = 27
+        oldMovement.driveRobotPosition(1.0, -(backDistance-hubLineup), true)
+//        same as at the top
+        oldMovement.driveRobotStrafe(0.8, 25.0, true)
+        oldMovement.driveRobotTurn(1.0, preloadTurn, true)
+        depo.moveToPosition(LiftPos.HighGoal.counts, 4500)
+        hardware.dropperServo.position = DropperPos.Open.posValue
+        sleep(100)
+        hardware.dropperServo.position = DropperPos.Closed.posValue
+        depo.moveToPosition(
+            yPosition = LiftPos.HighGoal.counts,
+            xPosition = depo.xFullyRetracted)
+        depo.moveToPosition(
+            yPosition = depo.fullyDown,
+            xPosition = depo.xFullyRetracted)
     }
 
 }
